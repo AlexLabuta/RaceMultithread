6cf159137c4a812fb9198b7053304dad2a204a16
Aleksandr <weteradaye@gmail.com>
2022-07-14 05:51:18 +0500

    Use atomic boolean in Car.java

--- src/main/java/edu/homework/multithread/Car.java
+++ src/main/java/edu/homework/multithread/Car.java
@@ -1,46 +1,50 @@
 package edu.homework.multithread;
 
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Car implements Runnable{
+public class Car implements Runnable {
     private static int CARS_COUNT;
-    private static final AtomicInteger aiForWinner;
-    private CyclicBarrier cb;
-    private Race race;
-    private int speed;
-    private String name;
+    private static final AtomicBoolean abForWinner;
 
     static {
         CARS_COUNT = 0;
-        aiForWinner = new AtomicInteger(0);
+        abForWinner = new AtomicBoolean(false);
     }
 
-    public String getName () {
-        return name;
-    }
-    public int getSpeed () {
-        return speed;
-    }
-    public Car (Race race, int speed, CyclicBarrier cb) {
-        this .race = race;
-        this .speed = speed;
+    private final CyclicBarrier cb;
+    private final Race race;
+    private final int speed;
+    private final String name;
+
+    public Car(Race race, int speed, CyclicBarrier cb) {
+        this.race = race;
+        this.speed = speed;
         CARS_COUNT++;
-        this .name = "Участник #" + CARS_COUNT;
+        this.name = "Участник #" + CARS_COUNT;
         this.cb = cb;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
     @Override
-    public void run () {
+    public void run() {
         try {
-            System.out.println( this .name + " готовится" );
-            Thread.sleep( 500 + ( int )(Math.random() * 800 ));
-            System.out.println( this .name + " готов" );
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
             cb.await();
             cb.await();
-            for ( int i = 0 ; i < race.getStages().size(); i++) {
-                race.getStages().get(i).go( this );
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
             }
-            if(aiForWinner.incrementAndGet() == 1){
+            if (abForWinner.compareAndSet(false, true)) {
                 System.out.println(name + " WIN!!!");
             }
             cb.await();
